39dcbda7913d85896b956f09a73215a1d3289ce0
Koushik Nandi <knandi020@gmail.com>
2025-07-05 12:36:09 +0530

    add one resource

---
+++ DevopsClass01/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "cc9c7c96-1363-4c58-85fc-fba96ba57b88"
+}
+
+resource "azurerm_resource_group" "kn1" {
+  name     = "kn1"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "kn11" {
+  name                     = "knstac11"
+  resource_group_name      = "kn1"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
