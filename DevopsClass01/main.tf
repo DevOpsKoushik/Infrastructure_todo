c9477cf2f6a32b63df9b1ec8bacc2da76fec067e
DevOpsKoushik <knandi020@gmail.com>
2025-07-29 10:38:21 +0530

    Update main.tf

--- DevopsClass01/main.tf
+++ DevopsClass01/main.tf
@@ -3,23 +3,24 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.27.0"
+      version = "4.37.0"
     }
   }
 }
 provider "azurerm" {
   features {}
-  subscription_id = "cc9c7c96-1363-4c58-85fc-fba96ba57b88"
+  subscription_id = "48959946-2f7e-4d0f-8c80-ed7b8706e6c3"
 }
 
-resource "azurerm_resource_group" "kn1" {
-  name     = "kn1"
+resource "azurerm_resource_group" "rg" {
+  name     = "knpipeline1"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "kn11" {
-  name                     = "knstac11"
-  resource_group_name      = "kn1"
+resource "azurerm_storage_account" "storage" {
+depends_on                = [azurerm_resource_group.rg]
+  name                     = "knstac"
+  resource_group_name      = "knpipeline1"
   location                 = "East US"
   account_tier             = "Standard"
   account_replication_type = "LRS"
